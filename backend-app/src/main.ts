f32d2cb5b508649ac24d65abd24c00dbdf3a5a80
Lucas <lucasramos1123.lr29@gmail.com>
2024-10-05 22:16:24 -0300

    feat: config inicial e instalação de dependencias

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  try {
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
+
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({});
+
+    await app.listen(3001);
+  } catch (error) {
+    console.error("Erro ao conectar com o banco de dados:", error);
+  }
 }
 bootstrap();
