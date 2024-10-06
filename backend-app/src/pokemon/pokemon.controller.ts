1362d06f3d5f437fd1e8046fce63860991efae1e
Lucas <lucasramos1123.lr29@gmail.com>
2024-10-05 22:26:45 -0300

    feat: api para capturar um pokemon

--- backend-app/src/pokemon/pokemon.controller.ts
+++ backend-app/src/pokemon/pokemon.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
+import { Repository } from 'typeorm';
+import { PokemonEntity } from '../pokemon/entities/pokemon.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('pokemon')
 export class PokemonController {
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor(
+    private readonly pokemonService: PokemonService,
+
+    @InjectRepository(PokemonEntity)
+    private readonly pokemonRepository: Repository<PokemonEntity>
+  ) {}
 
   @Get()
   async getPokemons() {
     return this.pokemonService.getPokemonRandon();
   }
 
+  @Post('capture')
+  async capturePokemon(@Body() pokemon: PokemonEntity): Promise<PokemonEntity> {
+      return this.pokemonService.capturePokemon(pokemon);
+  }
 }
--- backend-app/src/pokemon/pokemon.service.ts
+++ backend-app/src/pokemon/pokemon.service.ts
@@ -22,11 +22,23 @@ export class PokemonService {
         const reposnse = await this.httpService.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).pipe(map(response => response.data)).toPromise();
         
         const pokemon: PokemonEntity = {
-            name: reposnse.name,
-            id: reposnse.id,
-            photo: reposnse.sprites.front_default,
-            types: reposnse.types.map((type) => type.type.name)
-        };
-        return pokemon;
+          id: undefined,
+          name: reposnse.name,
+          pokemonId: reposnse.id,
+          photo: reposnse.sprites.front_default,
+          types: reposnse.types.map((type) => type.type.name)
+      };
+      return pokemon;
   }
+
+  async capturePokemon(pokemon: PokemonEntity): Promise<PokemonEntity> {
+      const newPokemon = this.pokemonRepository.create({
+          name: pokemon.name,
+          pokemonId: pokemon.pokemonId,
+          photo: pokemon.photo,
+          types: pokemon.types
+      });
+      return await this.pokemonRepository.save(newPokemon);
+  }
+
 }
