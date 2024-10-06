35a9df0dcc5e8518dd782da710840b54d2986873
Lucas <lucasramos1123.lr29@gmail.com>
2024-10-05 22:50:22 -0300

    feat: criação da pagina inicial

--- frontend-app/src/app/page.tsx
+++ frontend-app/src/app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
+'use client'
+
 import styles from "./page.module.css";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/');
+  };
+
   return (
     <div className={styles.page}>
-      
+      <h1 >Bem-vindo ao Mundo Pokémon!</h1>
+      <button onClick={handleClick}>Encontrar Pokémons</button>
     </div>
   );
 }
