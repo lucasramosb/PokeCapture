d6548549e63f34c709b654a44d7c736413de00dc
Lucas <lucasramos1123.lr29@gmail.com>
2024-10-05 22:56:26 -0300

    feat: criação do componente reutilizavel de container da aplicação

---
+++ frontend-app/src/app/components/container/container.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import styles from './container.module.css';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children}) => {
+  return (
+    <div className={styles.container}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
